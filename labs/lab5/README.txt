7d2eca1f9cadef200988a8b9af53bd8796af59eb
hamlim <hamlim@rpi.edu>
2014-03-23 18:04:46 -0400

    Lab 5 & deleted content
    more labs and more deleted content

---
+++ labs/lab5/README.txt
@@ -0,0 +1,8 @@
+Over all the lab was pretty simple, although File system on Nodejs was a bit of a pain given that I did not properly read that fs.writefile() 
+would overwrite the contents in a file, so I ended up looking at my file and only finding one json object for a tweet not the expected amounts
+I was hoping for. 
+
+Other than that i spent more time making the file of tweets itself and transfering it over to a zip folder because of the amount of content we 
+had to find through the twitter api.
+
+I used nTwitter to get all the content, it isn't that bad, but I am surprised that the count parameter does not exist for nTwitter.
--- labs/lab5/start.js
+++ labs/lab5/start.js
@@ -1,29 +1,26 @@
+var jf = require('jsonfile');
+var fs = require('fs');
+
 var twitter = require('ntwitter');
-var jf = require('jsonfile')
-
-
 
 var twit = new twitter({
-		consumer_key: "zV4V7BI1TainNbCqcvNukQ",
-		consumer_secret: "jsGEvqIZ95yBiBOTx84TpXTxuDUQiuLZvpvSzO87Lo",
-		access_token_key: "118285131-SPuBN9vASoUW8WGPcci9LQq42LTDiJgqVLf1po11",
-		access_token_secret: "xOcMOh5wbIl8vZpbue8xlwhsWOXPqzUCYiSzXr3nLAgch"
-});
-var count = 0;
-var file = "tweets.json"
-//streaming api
-twit.stream('statuses/sample', function(stream) {
-	stream.on('data', function(data) {
-		if(count < 10){
-			count = count + 1;
-			console.log(count);
-			jf.writeFile("10tweets-tweets.json", data, function(err) {
-  			console.log(err);
-			});
-		}
-		else {
-			stream.destroy;
-		}
-	});
+  consumer_key: "zV4V7BI1TainNbCqcvNukQ",
+  consumer_secret: "jsGEvqIZ95yBiBOTx84TpXTxuDUQiuLZvpvSzO87Lo",
+  access_token_key: "118285131-SPuBN9vASoUW8WGPcci9LQq42LTDiJgqVLf1po11",
+  access_token_secret: "xOcMOh5wbIl8vZpbue8xlwhsWOXPqzUCYiSzXr3nLAgch"
 });
 
+var count = 0;
+var log = fs.createWriteStream('nTwitter - tweets.json', {'flags': 'a'});
+
+var sw='-73.68,42.72', ne='-73.67,42.73'; //  RPI
+twit.stream('statuses/filter', {'locations':sw +','+ne},
+function(stream) {
+  stream.on('data', function (data) {
+		if(count < 1000){
+			log.write(JSON.stringify(data));
+		};
+		count = count + 1;
+		console.log(count); //keep track of the count
+  });
+});
